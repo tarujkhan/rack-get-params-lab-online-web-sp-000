1b5d5cee923d0694206a39585c330e5a07cbb019
taroj khan <tarujkhan@yahoo.com>
2020-09-23 21:41:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -75,7 +75,7 @@ class Application
         @@cart << search_term
         resp.write "added #{search_term}"
       else
-        resp.write "We don't have that item"
+        resp.write "We don't have that #{search_term}"
       end
    else
       resp.write "Path Not Found"
