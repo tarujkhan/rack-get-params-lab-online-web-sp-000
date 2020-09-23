85dc1330c1bc46f132f93ceb9f6a5cb1e67a11d5
taroj khan <tarujkhan@yahoo.com>
2020-09-23 19:45:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,7 +27,7 @@ elsif req.path.match(/add/)
 
 search_term = req.params["q"]
 
-if @@items.include ? (search_term)
+if @@items.include? (search_term)
 @@cart << item
 @@cart
 else
@@ -38,7 +38,7 @@ resp.finish
 end
 
 def handle_search(search_term)
-if @@items.include ? (search_term)
+if @@items.include? (search_term)
 return "#{search_term} is one of our items"
 else
   return "Couldn't find #{search_term}"
