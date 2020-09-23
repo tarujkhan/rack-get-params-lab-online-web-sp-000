1dd7c118fcef3239cb80ad730678f21a79c01752
taroj khan <tarujkhan@yahoo.com>
2020-09-23 19:43:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,79 +1,47 @@
 class Application
 
-  @@items = ["Apples","Carrots","Pears"]
-  @@cart = []
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+@ @items = ["Apples", "Carrots", "Pears"]
+@ @cart = []
+def call(env)
+resp = Rack::Response.new
+req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-    if req.path.match(/cart/)
-   if @@cart.empty?
-      resp.write "Your cart is empty"
+if req.path.match(/items/)
+@ @items.each do |item |
+    resp.write "#{item}\n"
+  if req.path.match(/cart/)
+if @ @cart.empty ?
+  resp.write "Your cart is empty"
 else
-      @@cart.each do |c|
-        resp.write "#{c}\n"
-      end 
-
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    else
-      resp.write "Path Not Found"
-    end
-      elsif req.path.match(/add/)
- 
-      search_term = req.params["q"]
- 
-      if @@items.include?(search_term)
-        @@cart << item
-        @@cart
-      else
-        resp.write "Your cart is empty"
-      end
-
-    resp.finish
+  @ @cart.each do |c |
+    resp.write "#{c}\n"
   end
 
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
+elsif req.path.match(/search/)
+search_term = req.params["q"]
+resp.write handle_search(search_term)
+else
+  resp.write "Path Not Found"
+end
+elsif req.path.match(/add/)
+
+search_term = req.params["q"]
+
+if @ @items.include ? (search_term)
+@ @cart << item
+@ @cart
+else
+  resp.write "Your cart is empty"
 end
 
+resp.finish
+end
 
-
-#   @@cart = []
- 
-#   # def call(env)
-#   #   resp = Rack::Response.new
-#   #   req = Rack::Request.new(env)
-#   if req.path.match(/cart/)
-#   if @@cart.empty?
-#       resp.write "Your cart is empty"
-# else
-#       @@cart.each do |c|
-#         resp.write "#{c}\n"
-#       end 
-# end
-
-#     elsif req.path.match(/add/)
- 
-#       search_term = req.params["q"]
- 
-#       if @@items.include?(search_term)
-#         @@cart << item
-#         @@cart
-#       else
-#         resp.write "Your cart is empty"
-#       end
- 
-#     resp.finish
-#   end
-# end
-
+def handle_search(search_term)
+if @ @items.include ? (search_term)
+return "#{search_term} is one of our items"
+else
+  return "Couldn't find #{search_term}"
+end
+end
+end
